52f2c8f34017dc82a8708dde0f5e194d7cc31e6a
Trevor Cooper <tcooper@sdsc.edu>
2013-11-14 15:34:01 -0800

    Edited src/pgi/[binary_hashes|Makefile|version.mk] to support pull.mk

--- src/pgi/version.mk
+++ src/pgi/version.mk
@@ -1,3 +1,13 @@
-NAME    = pgi
-VERSION = 13.2
-RELEASE = 0
+NAME            = pgi
+VERSION         = 13.2
+RELEASE         = 0
+
+SRC_SUBDIR      = pgi 
+
+PGI_NAME        = pgi
+PGI_VERSION     = $(VERSION)
+PGI_PKG_SUFFIX  = tar.gz
+PGI_SOURCE_PKG  = $(PGI_NAME)-$(PGI_VERSION).$(PGI_PKG_SUFFIX)
+PGI_SOURCE_DIR  = $(PGI_SOURCE_PKG:%.$(PGI_PKG_SUFFIX)=%)
+
+TAR_GZ_PKGS     = $(PGI_SOURCE_PKG)
