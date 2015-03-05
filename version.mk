3b505fba9f7a380883c8cec188dedf76916dc361
Jerry Greenberg <jpg@comet-devel.local>
2015-03-05 13:14:28 -0800

    added roll version stuff

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = pgi
-RELEASE = 0
-COLOR   = orange
+ROLLNAME	= pgi
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR		= orange
 
 REDHAT.ROOT = $(PWD)
