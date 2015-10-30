09da78bb3cdf5ea470ee1acd79dbe09752f6cf3f
Jim Hayes <jhayes@sdsc.edu>
2015-10-30 09:19:26 -0700

    Modify download to work with unrepackaged tar ball. Resolves APS-210.

--- src/pgi/version.mk
+++ src/pgi/version.mk
@@ -1,13 +1,13 @@
 NAME           = sdsc-pgi
 VERSION        = 15.7
-RELEASE        = 0
+RELEASE        = 1
 PKGROOT        = /opt/pgi
 
 SRC_SUBDIR     = pgi
 
-SOURCE_NAME    = pgi
+SOURCE_NAME    = pgilinux
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = $(VERSION)
+SOURCE_VERSION = 2015-$(VERSION)-x86_64
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
