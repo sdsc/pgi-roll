2a064e090a09c0b1dccfcf5baec0eaed2e518af1
Jerry Greenberg <jpg@sdsc.edu>
2017-06-21 15:06:00 -0700

    APS-497 pgi 17.5

--- src/pgi/version.mk
+++ src/pgi/version.mk
@@ -1,15 +1,15 @@
 NAME           = sdsc-pgi
-VERSION        = 15.7
-RELEASE        = 1
+VERSION        = 17.5
+RELEASE        = 0
 PKGROOT        = /opt/pgi
 
 SRC_SUBDIR     = pgi
 
 SOURCE_NAME    = pgilinux
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = 2015-$(VERSION)-x86_64
+SOURCE_VERSION = 2017-175-x86_64
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_DIR     = install_components
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
